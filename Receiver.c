0b1e977fd681047a19693ea7cdd364bc848e150b
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 <royx520@gmail.com>
2022-12-05 13:07:05 +0200

    version2

--- Receiver.c
+++ Receiver.c
@@ -1,1 +1,52 @@
 #include <stdio.h>
+#include <errno.h> 
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <string.h> 
+#include <arpa/inet.h>
+#include <stdlib.h> 
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define SERVER_PORT 9999
+
+int main(){
+
+    int listensocket = -1;
+
+    if((listensocket = socket(AF_INET , SOCK_STREAM , 0 )) == -1)
+    {
+        printf("Could not create listening socket : %d" ,errno);
+    }
+
+    int yes=1; 
+    if (setsockopt(listensocket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1)
+    { 
+	    perror("setsockopt"); 
+	    exit(1);
+    }
+    struct sockaddr_in serverAddress;
+    memset(&serverAddress, 0, sizeof(serverAddress));
+
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(SERVER_PORT);  
+
+    if (bind(listensocket, (struct sockaddr *)&serverAddress , sizeof(serverAddress)) == -1)
+    {
+        printf("Bind failed with error code : %d" ,errno);
+        close(listensocket);
+        return -1;
+    }
+      
+    printf("Bind success\n");
+  
+    if (listen(listensocket, 1) == -1) 
+	{	
+	printf("listen failed with error code : %d",errno);
+	    close(listensocket);
+        return -1;
+    }
+    
+    return 0;
+}
--- Sender.c
+++ Sender.c
@@ -1,1 +1,50 @@
 #include <stdio.h>
+#include <errno.h> 
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <string.h> 
+#include <arpa/inet.h>
+#include <stdlib.h> 
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define SERVER_PORT 9999
+#define SERVER_IP_ADDRESS "0.0.0.0"
+#define SENDFILE "./text.txt"
+
+int main(){
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock==-1)
+    {
+        printf("Unable to create a socket : %d",errno);
+    }
+
+    struct sockaddr_in serverAddress;
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(SERVER_PORT);
+	int rval = inet_pton(AF_INET, (const char*)SERVER_IP_ADDRESS, &serverAddress.sin_addr);
+	if (rval <= 0)
+	{
+		printf("inet_pton failed");
+		return -1;
+	}
+
+    if (connect(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) == -1)
+     {
+	   printf("connect failed with error code : %d",errno);
+     }
+
+    printf("connected to server\n");
+
+    char *filename = SENDFILE; 
+    if (filename == NULL)
+    {
+        perror("Can't get filename");
+        exit(1);
+    }
+
+
+    close(sock);
+    return 0;
+}
